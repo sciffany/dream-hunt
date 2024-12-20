88842f985b1f7e43536fa7d1bf16bc28fc4642a0
Tiffany Chong <sciffany@gmail.com>
2024-12-20 19:17:36 +0800

    Add database url

--- gph/settings/base.py
+++ gph/settings/base.py
@@ -122,7 +122,7 @@ ASGI_APPLICATION = 'gph.asgi.application'
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.environ.get('DATABASE_URL'),
     }
 }
 
