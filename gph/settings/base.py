8e78f83584de38770b53b6af3ef58bb75514a2f5
Tiffany Chong <sciffany@gmail.com>
2024-12-20 10:14:10 +0800

    Add middleware

--- gph/settings/base.py
+++ gph/settings/base.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.contrib.contenttypes.middleware.ContentTypeMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
