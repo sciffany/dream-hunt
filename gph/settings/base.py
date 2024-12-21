f1721b19803d849d40f87fceb61a3788b8343457
Tiffany Chong <sciffany@gmail.com>
2024-12-21 12:42:55 +0800

    Puzzles

--- gph/settings/base.py
+++ gph/settings/base.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'puzzles.apps.PuzzlesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
